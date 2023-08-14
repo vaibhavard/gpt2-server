af22724652811e99008d43b9e6aa65594f23cfe3
Metaverseforpc <57561743+vaibhavard@users.noreply.github.com>
2023-08-14 20:38:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ chatbot = Chatbot(config={
   "access_token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJha2lrby50ZWNoaUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZX0sImh0dHBzOi8vYXBpLm9wZW5haS5jb20vYXV0aCI6eyJ1c2VyX2lkIjoidXNlci1ZdmFzTXRWNkczV3Q1aVd0UXhYVW5jZ1IifSwiaXNzIjoiaHR0cHM6Ly9hdXRoMC5vcGVuYWkuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA5NjAxMDY2NzU4Mzc5MjMwOTM0IiwiYXVkIjpbImh0dHBzOi8vYXBpLm9wZW5haS5jb20vdjEiLCJodHRwczovL29wZW5haS5vcGVuYWkuYXV0aDBhcHAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY5MDg3NzM0OSwiZXhwIjoxNjkyMDg2OTQ5LCJhenAiOiJUZEpJY2JlMTZXb1RIdE45NW55eXdoNUU0eU9vNkl0RyIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgbW9kZWwucmVhZCBtb2RlbC5yZXF1ZXN0IG9yZ2FuaXphdGlvbi5yZWFkIG9yZ2FuaXphdGlvbi53cml0ZSBvZmZsaW5lX2FjY2VzcyJ9.fIV2pRadJF1t4bHksskAfLd6VcPWol51LdRkwn1w1K-sqiA2OMCxW82LhkEdrdBORzRcWcHojNIYBhE0tufXqPVG6xWd08oyQ7SW4YKwLURUmOZtYXm4_uMu8_u0zTKUVkxZ2mVBJHiu4yPvcnZ_wGIqu3UfS3bB8nlHtl7eovzG1C6aVE3mDR8lXB2F9Iyyw8tmxZz-xG__dthnHJF1CIQnCssACV-rYvyJXWgtiStvJhRoLrnc4SF-ER0qRFphxolqCApIhUJL1B880G62MAh5-mOTthOJp5nnHY7qggNubv5Rth0yo8Txk8BMNlLwQT-v8nX8daOWLTWzW1g1MA"
 })
 
+import re
+def extract_links(string):
+    pattern = r'(https?://\S+)'
+    links = re.findall(pattern, string)
+    return links
 
 def post_requests():
     global ans
@@ -72,7 +77,7 @@ def chat_completions():
             print(reply)
             yield 'data: %s\n\n' % json.dumps(streamer(reply), separators=(',' ':'))
         yield 'data: %s\n\n' % json.dumps(streamer("\n\n"), separators=(',' ':'))
-        yield 'data: %s\n\n' % json.dumps(streamer("> GPT 4 response:"), separators=(',' ':'))
+        yield 'data: %s\n\n' % json.dumps(streamer("> GPT-4 Response: Thinking"), separators=(',' ':'))
 
         while ans == {}:
 
@@ -90,6 +95,7 @@ def chat_completions():
         completion_timestamp = int(time.time())
         completion_id = ''.join(random.choices(
             'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', k=28))
+        links = extract_links(json_body['response'])
 
         completion_data = {
             'id': f'chatcmpl-{completion_id}',
@@ -99,7 +105,7 @@ def chat_completions():
             'choices': [
                 {
                     'delta': {
-                        'content': "\n" + json_body['response']
+                        'content':  json_body['response']
                     },
                     'index': 0,
                     'finish_reason': None
@@ -108,6 +114,11 @@ def chat_completions():
         }
 
         yield 'data: %s\n\n' % json.dumps(completion_data, separators=(',' ':'))
+
+        aaa = 1
+        print(links)
+
+
 
         ans={}
 
@@ -124,15 +135,16 @@ def chat_completions():
 
 @app.route('/api/<name>')
 def hello_name(name):
+   global api_endpoint
    url = "https://"+name+"/conversation"
    api_endpoint=url
-   return f'{url}'
+   return f'{api_endpoint}'
 
 
 
 @app.route('/')
 def yellow_name():
-    return "API is UP and running."
+   return f'{api_endpoint}'
 
 @app.route("/v1/models")
 def models():
@@ -142,3 +154,11 @@ def models():
 
 
 
+if __name__ == '__main__':
+    config = {
+        'host': '0.0.0.0',
+        'port': 1337,
+        'debug': True
+    }
+
+    app.run(**config)
