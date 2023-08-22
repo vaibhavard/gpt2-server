76cf895045a062273cc035210c8c9c5d187ef0fb
Metaverseforpc <57561743+vaibhavard@users.noreply.github.com>
2023-08-22 18:48:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def send_req():
                     print(ddgd)
 
                     nline=True
-                time.sleep(0.22)
+                time.sleep(0.4)
 
                 worded=worded+token
             elif line and "conversationId"  in line.decode():
