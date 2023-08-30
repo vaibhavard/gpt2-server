18cbf17369f1f621d20b95c11ff9ccea16c282c9
Metaverseforpc <57561743+vaibhavard@users.noreply.github.com>
2023-08-31 00:33:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ chatbot = Chatbot(config={
 })
 
 
+
 def extract_links(text):
     # Regular expression pattern to match URLs
     url_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
