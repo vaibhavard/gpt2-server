8683decf991b76b12f90fd82366eac6374624e6d
Metaverseforpc <57561743+vaibhavard@users.noreply.github.com>
2023-09-21 10:47:42 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -12,7 +12,7 @@ processed_text=""
 nline = False
 ans={}
 #g4f.Provider.Yqcloud,
-providers=[g4f.Provider.Aivvm,g4f.Provider.Ails]
+providers=[ g4f.Provider.Yqcloud,g4f.Provider.Aivvm,g4f.Provider.Ails]
 systemp=False
 initial_instruction="Create a {help} on the below topic:\n"
 type_flowchart="""
